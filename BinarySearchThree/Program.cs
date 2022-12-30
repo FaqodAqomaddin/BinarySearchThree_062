eeb49b60ebc304e158ce497fb3b4aa49823f312f
FaqodAqomaddin <faqod.aqomaddin.ft21@mail.umy.ac.id>
2022-12-30 17:27:29 +0700

    add method insert

--- BinarySearchThree/Program.cs
+++ BinarySearchThree/Program.cs
@@ -28,5 +28,31 @@ namespace BinarySearchThree
         {
             ROOT = null; /*initializing ROOT to null */
         }
+        public void insert(string element)/*Insert a node in the binary search tree*/
+        {
+            Node tmp, parent = null, currentNode = null;
+            Search(element, ref parent, ref currentNode);
+            if(currentNode != null) /*check if the node to be inserted already inserted or not*/
+            {
+                Console.WriteLine("Duplicate words not allowed");
+                return;
+            }
+            else /*if the spesified node is not parent */
+            {
+                tmp = new Node(element, null, null); /*crates a node*/
+                if (parent == null) /*if the trees is empty */
+                {
+                    ROOT = tmp;
+                }
+                else if (string.Compare(element, parent.info) < 0)
+                {
+                    parent.leftchild = tmp;
+                }
+                else
+                {
+                    parent.rightchild = tmp;
+                }
+            }
+        }
     }
 }
