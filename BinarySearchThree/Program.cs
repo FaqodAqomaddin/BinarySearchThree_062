b7bde2633ff66c30340dcd64b567fff27b51db8b
FaqodAqomaddin <faqod.aqomaddin.ft21@mail.umy.ac.id>
2022-12-30 17:16:38 +0700

    Add clas Node and method node

--- BinarySearchThree/Program.cs
+++ BinarySearchThree/Program.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace BinarySearchThree
 {
-    class Program
+    class Node
     {
-        static void Main(string[] args)
+        public string info;
+        public Node leftchild;
+        public Node rightchild;
+
+        //Constructor for the node class
+        public Node(string i, Node l, Node r)
         {
+            info = i;
+            leftchild = l;
+            rightchild = r;
         }
     }
 }
