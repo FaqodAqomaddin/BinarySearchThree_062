6db1cd98184335e66e66d1d808300a308f62ff31
FaqodAqomaddin <faqod.aqomaddin.ft21@mail.umy.ac.id>
2022-12-30 17:59:29 +0700

    add menu

--- BinarySearchThree/Program.cs
+++ BinarySearchThree/Program.cs
@@ -32,7 +32,7 @@ namespace BinarySearchThree
         {
             Node tmp, parent = null, currentNode = null;
             search(element, ref parent, ref currentNode);
-            if(currentNode != null) /*check if the node to be inserted already inserted or not*/
+            if (currentNode != null) /*check if the node to be inserted already inserted or not*/
             {
                 Console.WriteLine("Duplicate words not allowed");
                 return;
@@ -59,7 +59,7 @@ namespace BinarySearchThree
             /*This function searches the currentNode of the specified Node as well as the current Node of its parent */
             currentNode = ROOT;
             parent = null;
-            while ((currentNode !=null) && (currentNode.info != element))
+            while ((currentNode != null) && (currentNode.info != element))
             {
                 parent = currentNode;
                 if (string.Compare(element, currentNode.info) < 0)
@@ -103,11 +103,59 @@ namespace BinarySearchThree
                 Console.WriteLine("Tree is empty");
                 return;
             }
-            if(ptr != null)
+            if (ptr != null)
             {
                 postorder(ptr.leftchild);
                 postorder(ptr.rightchild);
                 Console.Write(ptr.info + " ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            BinaryTree x = new BinaryTree();
+            while (true)
+            {
+                Console.WriteLine("\nMenu");
+                Console.WriteLine("1. Implement insert operation");
+                Console.WriteLine("2. Perform inorder traversal");
+                Console.WriteLine("3. Perform preorder traversal");
+                Console.WriteLine("4. Perform postorder traversal");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("\nEnter your choise (1-5) : ");
+                char ch = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine();
+                switch (ch)
+                {
+                    case '1':
+                        {
+                            Console.Write("Enter a word: ");
+                            String word = Console.ReadLine();
+                            x.insert(word);
+                        }
+                        break;
+                    case '2':
+                        {
+                            x.inorder(x.ROOT);
+                        }
+                        break;
+                    case '3':
+                        {
+                            x.preorder(x.ROOT);
+                        }
+                        break;
+                    case '4':
+                        {
+                            x.postorder(x.ROOT);
+                        }
+                        break;
+                    case '5':
+                        return;
+                    default:
+                        {
+                            Console.WriteLine("Invalid option");
+                            break;
+                        }
+                }
             }
         }
     }
